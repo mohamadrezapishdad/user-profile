703b6f0a6369733698f720a4478aafc4360bdc5c
Mohamadreza <Mohamadreza@DESKTOP.com>
2019-01-10 21:38:24 +0330

    User service can filter through users

--- src/Concerns/hasProfile.php
+++ src/Concerns/hasProfile.php
@@ -1,17 +1,19 @@
 <?php
 namespace Larafa\UserProfile;
 
+use Larafa\UserProfile\Profile;
+
 trait hasProfile {
 	
 	public function profile()
     {
-        return $model->hasOne(\App\Profile::class, 'id', 'user_id');
+        return $this->belongsTo(Profile::class , 'id', 'user_id');
     }
 
     public static function create(array $attributes = [])
     {
         $user = static::query()->create($attributes);
-        \Larafa\UserProfile\Profile::create([
+        Profile::create([
             'user_id'=> $user->id
         ]);
         return $user;
